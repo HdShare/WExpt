929a3227598a60f54abf4119bd853e88ba4049a5
hd <hdshare@vip.qq.com>
2025-12-07 19:04:49 +0800

    bump version 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "me.hd.wexpt"
         minSdk = 27
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
         buildConfigField("String", "APP_NAME", "\"WExpt\"")
     }
     androidResources {
